197e08d02f297499ba1500715c648c48305fc255
Enrico700 <70644266+Enrico700@users.noreply.github.com>
2021-07-06 00:39:11 -0300

    Update DelayAndSumMain.py

--- DelayAndSumMain.py
+++ DelayAndSumMain.py
@@ -2,22 +2,21 @@ import soundfile as sf
 import numpy as np
 from beamformer import util
 from beamformer import delayandsum as ds
-
 input_SoundAngles = np.array([0, 60, 120, 180, 270, 330])
-desired_direction = 0
+desired_direction = 270
 mic_radius = 0.0922
 mic_diameter = 2*mic_radius
 fft_window = 1024
 fft_shift = 512
 sampling_freq = 16000
 
-def input_read(prefix=r'./sample_data/20G_20GO010I_STR.CH{}.wav',
-    channel_index=np.array([1, 2, 3, 4, 5, 6])):
-    wav, _ = sf.read(prefix.replace('{}', str(channel_index[0])), dtype='float32')
+def input_read(input_path=r'./inputs/Mic{N}_440Hz.wav',channel_index=np.array([1, 2, 3, 4, 5, 6])):
+    Input_WaveNames = input_path.replace('{N}', str(channel_index[0]))
+    wav, _ = sf.read(Input_WaveNames)
     channels_wav = np.zeros((len(wav), len(channel_index)), dtype=np.float32)
     channels_wav[:, 0] = wav
     for i in range(1, len(channel_index)):
-        channels_wav[:, i] = sf.read(prefix.replace('{}', str(channel_index[i])), dtype='float32')[0]
+        channels_wav[:, i] = sf.read(Input_WaveNames)[0]
     return channels_wav
 
 input_arrays = input_read()
