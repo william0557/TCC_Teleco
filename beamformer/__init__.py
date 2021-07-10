c5b5fddf52dd4e7d1cad09258f1ecebbcba626cc
Enrico700 <70644266+Enrico700@users.noreply.github.com>
2021-07-10 19:53:33 -0300

    Update __init__.py

--- beamformer/__init__.py
+++ beamformer/__init__.py
@@ -1,9 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Jan 16 15:12:35 2019
-
-@author: a-kojima
-"""
 from . import util
 from . import delayandsum
 from . import minimum_variance_distortioless_response
